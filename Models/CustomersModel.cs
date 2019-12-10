ea48de0abcdc602e263f987264240195d6ce4f10
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-12-10 15:42:20 +0530

    validation for add customer

--- Models/CustomersModel.cs
+++ Models/CustomersModel.cs
@@ -1,9 +1,13 @@
-﻿namespace Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
 {
     public class CustomersModel
     {
         public int Cid { get; set; }
+        [Required(ErrorMessage = "Please provide valid Name for Customer!")]
         public string Name { get; set; }
+        [Required]
         public string Telephone { get; set; }
         public string Email { get; set; }
     }
