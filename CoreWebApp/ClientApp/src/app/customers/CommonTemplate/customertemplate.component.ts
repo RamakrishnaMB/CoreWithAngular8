7adc338cdae1ace35f0fbb863780160f583c0677
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-12-11 17:59:31 +0530

    revert

---
+++ CoreWebApp/ClientApp/src/app/customers/CommonTemplate/customertemplate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customertemplate',
+  templateUrl: './customertemplate.component.html',
+  styleUrls: ['./customertemplate.component.css']
+})
+export class CustomertemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
