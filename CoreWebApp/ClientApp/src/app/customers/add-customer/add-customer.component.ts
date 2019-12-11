f3c1fc61ba320672cd2b7bdebf72d29158dfe22a
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-12-11 11:31:09 +0530

    angular 8 validation for add customer

--- CoreWebApp/ClientApp/src/app/customers/add-customer/add-customer.component.ts
+++ CoreWebApp/ClientApp/src/app/customers/add-customer/add-customer.component.ts
@@ -7,10 +7,41 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-customer.component.css']
 })
 export class AddCustomerComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.registerForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      name: ['', Validators.required],
+      telephone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      acceptTerms: [false, Validators.requiredTrue]
+    });
+
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
   }
 
 }
