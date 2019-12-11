7e333fdeb120ffd5e85ce677d9d4496f594fbc87
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-12-11 15:48:57 +0530

    code clean

--- CoreWebAPIServices/Controllers/CustomersController.cs
+++ CoreWebAPIServices/Controllers/CustomersController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Formatting;
 using System.Threading.Tasks;
 using CoreBusinessLogic.Interface;
 using CoreDataLayer.ModelsDB;
@@ -27,20 +24,12 @@ namespace CoreWebAPIServices.Controllers
         [Route("GetDetails")]
         public IActionResult GetFeeDetails()
         {
-            //http://localhost:59881/api/FeeDetailsAPI/GetFeeDetails
             //var DeviceId = ((JObject)jObject).GetValue("deviceId", StringComparison.OrdinalIgnoreCase).Value<string>();
             //var GradeId = ((JObject)jObject).GetValue("gradeId", StringComparison.OrdinalIgnoreCase).Value<int>();
             List<Customers> customers = this.customerService.GetCustomers();
-
             APIMessage aPIMessage = new APIMessage { StatusCode = (int)HttpStatusCode.OK, StatusMessage = "Sucess great !!", Data = customers };
-
             //method: post, url : http://localhost:58272/api/customers/getdetails , Request body: { userId:10,deviceId:11,gradeId:22} , Content-Type: application/json            
-
-
-
             return StatusCode((int)HttpStatusCode.OK, customers);
-
-
         }
 
         [HttpPost]
@@ -50,10 +39,8 @@ namespace CoreWebAPIServices.Controllers
             if (ModelState.IsValid)
             {
                 await this.customerService.AddCustomer(customer);
-             
             }
-
-            return StatusCode((int)HttpStatusCode.OK, customer);
+            return StatusCode((int)HttpStatusCode.Created, customer);
         }
 
     }
