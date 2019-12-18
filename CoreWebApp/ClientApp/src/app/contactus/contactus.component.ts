20e5976ff1f874c7fdfc81daefbfade9d290c47e
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-12-18 17:37:41 +0530

    contact us form placed in angular 8

--- CoreWebApp/ClientApp/src/app/contactus/contactus.component.ts
+++ CoreWebApp/ClientApp/src/app/contactus/contactus.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ContactUs } from '../Models/ContactUs';
 
 @Component({
   selector: 'app-contactus',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
+  contactUsForm: FormGroup;
+  contactUs: ContactUs;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private _router: Router) { }
 
   ngOnInit() {
+    this.contactUsForm = this.formBuilder.group({
+      Name: ['', Validators.required],
+      Email: ['', Validators.email, Validators.required],
+      Message: ['', Validators.required, Validators.minLength(100)]
+    });
   }
 
+  get f() {
+    return this.contactUsForm.controls;
+  }
+  onSubmit() {
+
+    if (this.contactUsForm.invalid) {
+      return;
+    }
+
+    this.contactUs = new ContactUs();
+    this.contactUs.Name = this.contactUsForm.value.Name;
+    this.contactUs.Email = this.contactUsForm.value.Email;
+    this.contactUs.Message = this.contactUsForm.value.Message;
+
+  }
 }
