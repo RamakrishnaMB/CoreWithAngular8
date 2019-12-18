bd6805df01ccd953aa6e144506b6dde61de8ff8d
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-12-18 15:55:28 +0530

    contact us page added with submit send email option

---
+++ CoreWebAppRazor/Controllers/ContactController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Configuration.EmailConfig;
+using Configuration.EmailConfig.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace CoreWebAppRazor.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IEmailService emailService;
+
+        public ContactController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(ContactUs contactUs)
+        {
+
+            EmailMessage emailMessage = new EmailMessage
+            {
+                Subject = "Request for Contact details from " + contactUs.Name,
+                Content = contactUs.Message + "</br>" + "Name " + contactUs.Name + "</br>" + "Email " + contactUs.Email + "</br>" + "",
+                FromAddresses = new List<EmailAddress> { new EmailAddress
+                {
+                    Address="findingRK@gmail.com"
+                }
+                },
+                ToAddresses = new List<EmailAddress> { new EmailAddress
+               {
+                   Name = "RK",
+                   Address = "ramakrishnamb@gmail.com",
+               }}
+            };
+            //this.emailService.Send(emailMessage);
+            ViewBag.result = "We will contact you soon!";
+            return View();
+        }
+    }
+}
---
+++ Models/ContactUs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class ContactUs
+    {
+        [Required(ErrorMessage = "Please provide valid name to contact you.")]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please provide message with in 50 words."), MaxLength(100)]
+        public string Message { get; set; }
+    }
+}
