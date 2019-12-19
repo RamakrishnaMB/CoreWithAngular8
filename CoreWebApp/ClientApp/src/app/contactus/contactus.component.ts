1e5a1e7025cd77c25859d592b2f66924a2dcebdc
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-12-19 15:48:08 +0530

    alert

--- CoreWebApp/ClientApp/src/app/contactus/contactus.component.ts
+++ CoreWebApp/ClientApp/src/app/contactus/contactus.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ContactUs } from '../Models/ContactUs';
 import { ContactusService } from '../Services/contactus.service';
+import { AlertService } from '../_alert';
 
 
 @Component({
@@ -16,7 +17,7 @@ export class ContactusComponent implements OnInit {
   contactUsForm: FormGroup;
   contactUs: ContactUs;
 
-  constructor(private formBuilder: FormBuilder, private _router: Router, private contactUsService: ContactusService) { }
+  constructor(private formBuilder: FormBuilder, private _router: Router, private contactUsService: ContactusService, private alertService: AlertService) { }
 
   ngOnInit() {
     this.contactUsForm = this.formBuilder.group({
@@ -30,20 +31,21 @@ export class ContactusComponent implements OnInit {
     return this.contactUsForm.controls;
   }
   contactSubmit() {
+    this.alertService.success("Email sent successfully!!","alert-1");
     debugger;
-    if (this.contactUsForm.invalid) {
-      return;
-    }
+    //if (this.contactUsForm.invalid) {
+    //  return;
+    //}
 
-    this.contactUs = new ContactUs();
-    this.contactUs.Name = this.contactUsForm.value.Name;
-    this.contactUs.Email = this.contactUsForm.value.Email;
-    this.contactUs.Message = this.contactUsForm.value.Message;
-    this.contactUsService.ContactUs(this.contactUs).subscribe(
-      (data: ContactUs) => {
-        console.log(data);
-        this.contactUsForm.reset();
-      }, (error: any) => { console.log(error) }
-    );
+    //this.contactUs = new ContactUs();
+    //this.contactUs.Name = this.contactUsForm.value.Name;
+    //this.contactUs.Email = this.contactUsForm.value.Email;
+    //this.contactUs.Message = this.contactUsForm.value.Message;
+    //this.contactUsService.ContactUs(this.contactUs).subscribe(
+    //  (data: ContactUs) => {
+    //    console.log(data);
+    //    this.contactUsForm.reset();
+    //  }, (error: any) => { console.log(error) }
+    //);
   }
 }
