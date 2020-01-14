07cf3a16ab3625f7b446d52a550cfba0f3090e89
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2020-01-14 12:48:08 +0530

    customer class updated in typescript

--- CoreWebApp/ClientApp/src/app/customers/Customer.js
+++ CoreWebApp/ClientApp/src/app/customers/Customer.js
@@ -6,4 +6,5 @@ var Customers = /** @class */ (function () {
     return Customers;
 }());
 exports.Customers = Customers;
+//copy from class to typescript works after installing visual studio extension - TypescriptSyntexPaste
 //# sourceMappingURL=Customer.js.map
--- CoreWebApp/ClientApp/src/app/customers/Customer.ts
+++ CoreWebApp/ClientApp/src/app/customers/Customer.ts
@@ -1,8 +1,10 @@
 export class Customers {
-
   public cid: number;
   public name: string;
   public telephone: string;
   public email: string;
-
+  public profilepic: string;
 }
+
+//copy from class to typescript works after installing visual studio extension - TypescriptSyntexPaste
+
