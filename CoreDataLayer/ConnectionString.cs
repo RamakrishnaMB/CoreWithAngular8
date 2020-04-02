2b6c205ac0e268cb61c8d91da59a74bcb7084267
Ramakrishna <ramakrishna.mb@excelindia.com>
2020-04-02 14:58:11 +0530

    db connection string for local updated

--- CoreDataLayer/ConnectionString.cs
+++ CoreDataLayer/ConnectionString.cs
@@ -6,6 +6,8 @@ namespace CoreDataLayer
 {
     public static class ConnectionString
     {
-        public const string  GetConnectionString = "Server=ES-SYS-764\\SQL2017;Database=dbTest;Trusted_Connection=True;";
+      //  public const string  GetConnectionString = "Server=ES-SYS-764\\SQL2017;Database=dbTest;Trusted_Connection=True;";
+      //home sql server
+        public const string GetConnectionString = "Server=DESKTOP-LBTN8H4\\SQLEXPRESS;Database=dbTest;Trusted_Connection=True;";
     }
 }
