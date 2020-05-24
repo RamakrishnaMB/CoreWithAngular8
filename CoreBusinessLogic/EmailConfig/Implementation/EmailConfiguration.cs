62299bcfa70af087f1d0147943c5408e5acae1bc
Ramakrishna <ramakrishnamb@gmail.com>
2020-05-25 01:22:53 +0530

    Update EmailConfiguration.cs

--- CoreBusinessLogic/EmailConfig/Implementation/EmailConfiguration.cs
+++ CoreBusinessLogic/EmailConfig/Implementation/EmailConfiguration.cs
@@ -11,11 +11,11 @@ namespace Configuration.EmailConfig.Implementation
         public string SmtpServer { get; set; } = "smtp.gmail.com";
         public int SmtpPort { get; set; } = 587;
         public string SmtpUsername { get; set; } = "ramakrishna.public@gmail.com";
-        public string SmtpPassword { get; set; } = "9880205856";
+        public string SmtpPassword { get; set; } = "0022555";
         //To recive emails from server
         public string PopServer { get; set; } = "pop.gmail.com";
         public int PopPort { get; set; } = 995;
         public string PopUsername { get; set; } = "ramakrishna.public@gmail.com";
-        public string PopPassword { get; set; } = "9880205856";
+        public string PopPassword { get; set; } = "0022555";
     }
 }
